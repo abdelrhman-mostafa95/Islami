721a9067710f642f73ee853d35272f481dc8d81a
Abdelrahman Mostafa <abdelrhman.mostafa1095@gmail.com>
2025-01-27 02:19:20 +0200

    rebase

---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_ar.dart
@@ -0,0 +1,53 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for Arabic (`ar`).
+class AppLocalizationsAr extends AppLocalizations {
+  AppLocalizationsAr([String locale = 'ar']) : super(locale);
+
+  @override
+  String get app_title => 'أسلامي';
+
+  @override
+  String get quran => 'قران';
+
+  @override
+  String get hadith => 'حديث';
+
+  @override
+  String get sebha => 'سبحة';
+
+  @override
+  String get radio => 'راديو';
+
+  @override
+  String get settings => 'الاعدادت';
+
+  @override
+  String get chapter_name => 'اسم السورة';
+
+  @override
+  String get verses_number => 'عدد الايات';
+
+  @override
+  String get ahadith => 'الاحاديث';
+
+  @override
+  String get theme => 'النمط';
+
+  @override
+  String get language => 'اللغة';
+
+  @override
+  String get light => 'فاتح';
+
+  @override
+  String get dark => 'داكن';
+
+  @override
+  String get english => 'الانجليزية';
+
+  @override
+  String get arabic => 'العربية';
+}
---
+++ .dart_tool/flutter_gen/gen_l10n/app_localizations_en.dart
@@ -0,0 +1,53 @@
+import 'app_localizations.dart';
+
+// ignore_for_file: type=lint
+
+/// The translations for English (`en`).
+class AppLocalizationsEn extends AppLocalizations {
+  AppLocalizationsEn([String locale = 'en']) : super(locale);
+
+  @override
+  String get app_title => 'Islami';
+
+  @override
+  String get quran => 'Quran';
+
+  @override
+  String get hadith => 'Hadith';
+
+  @override
+  String get sebha => 'Sebha';
+
+  @override
+  String get radio => 'Rdaio';
+
+  @override
+  String get settings => 'Settings';
+
+  @override
+  String get chapter_name => 'Chapter Name';
+
+  @override
+  String get verses_number => 'Verses Number';
+
+  @override
+  String get ahadith => 'Ahadith';
+
+  @override
+  String get theme => 'Theme';
+
+  @override
+  String get language => 'Language';
+
+  @override
+  String get light => 'Light';
+
+  @override
+  String get dark => 'Dark';
+
+  @override
+  String get english => 'English';
+
+  @override
+  String get arabic => 'Arabic';
+}
